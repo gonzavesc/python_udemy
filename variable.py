3371e8fc302be8b152e9ff5eadda4285e0b5eee3
Vescovi Pérez <gvescovi@indra.es>
2019-08-22 09:25:11 +0200

    15 strings and indexint

--- variable.py
+++ variable.py
@@ -8,3 +8,14 @@ my_income = 100
 tax_rate= 0.1
 my_taxes = my_income * tax_rate
 print(my_taxes)
+
+new_word = "hello"
+print(new_word)
+print(len(new_word))
+my_string = "Hello World"
+print(my_string[-2])
+print(my_string[2:])
+print(my_string[:3])
+print(my_string[3:6])
+print(my_string[::2]) #The start stop step size!
+print(my_string[::-1])
