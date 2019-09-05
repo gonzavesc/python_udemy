6e43f4519d1ba21ccc6f9ecd16600a29f7272bae
Vescovi Pérez <gvescovi@indra.es>
2019-09-05 16:24:00 +0200

    Changed project to functions

--- first_project.py
+++ first_project.py
@@ -7,51 +7,56 @@ def check_game(board):
 	diagonal = board[0]==board[4]==board[8]!=' '  or board[6]==board[4]==board[2]!=' ' 
 	return horizontal or vertical or diagonal
 cont = True
-choose = True
-while choose:
-	player_1 = input("Player 1 choose 'X' or 'O' ")
-	if player_1 not in 'XO' or player_1=='':
-		print("Choose 'X' or 'O' ")
-	else:
-		break
-if player_1 == 'X':
-	player_2 = 'O'
-else:
-	player_2 = 'X'
-cont = True
-myset = set()
-turn_1 = True
-while cont and len(myset)<9:
-	if turn_1:
-		good = False
-		while not good:
-			position = int(input("Player 1 choose position: "))
-			if position in myset:
-				print("Position is already chosen")
-			else:
-				good = True
-				myset.add(position)
-				board[position - 1] = player_1
-		turn_1 = False
-	else:
-		good = False
-		while not good:
-			position = int(input("Player 2 choose position: "))
-			if position in myset:
-				print("Position is already chosen")
-			else:
-				good = True
-				myset.add(position)
-				board[position - 1] = player_2
-		turn_1 = True
-	print_board(board)
-	cont = not check_game(board)
-	print(cont)
-	if not cont:
-		if turn_1:
-			print("The winner is player 2")
+def choose_player():
+	choose = True
+	while choose:
+		player_1 = input("Player 1 choose 'X' or 'O' ")
+		if player_1 not in 'XO' or player_1=='':
+			print("Choose 'X' or 'O' ")
 		else:
-			print("The winner is player 1")
-		
+			break
+	if player_1 == 'X':
+		player_2 = 'O'
+	else:
+		player_2 = 'X'
+	return player_1, player_2
 
+def start_game():
+	player_1, player_2 = choose_player()
+	cont = True
+	myset = set()
+	turn_1 = True
+	while cont and len(myset)<9:
+		if turn_1:
+			good = False
+			while not good:
+				position = int(input("Player 1 choose position: "))
+				if position in myset:
+					print("Position is already chosen")
+				else:
+					good = True
+					myset.add(position)
+					board[position - 1] = player_1
+			turn_1 = False
+		else:
+			good = False
+			while not good:
+				position = int(input("Player 2 choose position: "))
+				if position in myset:
+					print("Position is already chosen")
+				else:
+					good = True
+					myset.add(position)
+					board[position - 1] = player_2
+			turn_1 = True
+		print_board(board)
+		cont = not check_game(board)
+		print(cont)
+		if not cont:
+			if turn_1:
+				print("The winner is player 2")
+			else:
+				print("The winner is player 1")
+		
+start_game()
 print("Game over")
