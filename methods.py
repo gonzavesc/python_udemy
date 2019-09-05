c025f7a36a44696331f8577f65cc982242a27305
Vescovi Pérez <gvescovi@indra.es>
2019-09-05 12:41:30 +0200

    48 challenging problems

--- methods.py
+++ methods.py
@@ -132,3 +132,28 @@ def summer_69(arr):
 				break
 	return sum
 print(summer_69([1,2,3,4,5,6,7,8,9,10]))
+
+def spy_game(nums):
+	code = [0,0,7,'x']
+	for num in nums:
+		if num == code[0]:
+			code.pop(0)
+	return len(code)==1
+print(spy_game([0,0,7,12]))
+
+def count_primes(num):
+	if num < 2:
+		return 0
+	primes = [2]
+	x = 3
+	while x <= num:
+		for y in range(3,x,2):
+			if x%y == 0:
+				x += 2
+				break
+		else: # solo entra si no usa el break
+			primes.append(x)
+			x += 2
+	print(primes)
+	return len(primes)
+print(count_primes(2000))
